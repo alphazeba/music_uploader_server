3d72e2040ab85cc291f401c9d4c2f75e11ffdc2e
arnhom <arnhom@outlook.com>
2025-03-31 18:09:48 -0700

    fix: server env variable

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use activities::{simple_routes::{check_auth, check_conn}, trigger_scan::trigger_
 use authenticated::Authenticator;
 use config::server_config::ServerConfig;
 use rocket::{catch, catchers, fairing::AdHoc, routes, Build, Rocket};
+use std::env;
 
 pub mod model;
 mod path_utils;
@@ -16,6 +17,7 @@ fn unauthorized() -> String {
 }
 
 pub fn build_rocket() -> Rocket<Build> {
+    config_env_or_panic();
     let authenticator= Authenticator::new()
         .expect("cannot run server without authenticator must look into issues");
     rocket::build()
@@ -28,4 +30,10 @@ pub fn build_rocket() -> Rocket<Build> {
         ])
         .attach(AdHoc::config::<ServerConfig>())
         .manage(authenticator)
+}
+
+pub fn config_env_or_panic() {
+    let music_env = env::var("MUSIC_ENV").expect("MUSIC_ENV must be set");
+    let _ = env::set_current_dir(music_env.clone()).expect(&format!("Failed to parse MUSIC_ENV ({}) as path", music_env));
+        println!("current dir is: {:?}", env::current_dir());
 }
